deffcb6f7b62aa269daedad4566dd65ff7df6310
Lee Drake <b.lee.drake@gmail.com>
2018-03-20 12:16:15 -0600

    Update Test Features.R

--- Test Features.R
+++ Test Features.R
@@ -6,21 +6,26 @@ y.scale <- scales::rescale(y, to=c(0,1))
 
 scale.frame <- data.frame(
 x = c(1, 2, 3, 4, 5, 6, 7, 8),
-y = c(123, 75, 50, 30, 20, 10, 5, 2.5),
+y = c(123, 75, 50, 30, 20, 10, 5, 2.5)
+,
 scaleX = scales::rescale(x, to=c(0,1)),
 scaleY = scales::rescale(y, to=c(0,1))
 )
+
+independent <- "x"
+dependent <- "y"
+dataframe <- scale.frame
 
 
 screeCrunch <- function(dataframe, dependent, independent){
 
 simple.frame <- data.frame(
-newY = scales::rescale(dataframe[,dependent], to=c(0,1)), 
-newX = scales::rescale(dataframe[,independent], to=c(0,1)))
+newY = dataframe[,dependent]/max(dataframe[,dependent]),
+newX = dataframe[,independent]/max(dataframe[,independent]))
 
 sims <-data.frame( 
-sims1 = seq(from=1, to=nrow(dataframe)-2, by=1),
-sims2 = seq(from=3, to=nrow(dataframe), by=1)
+sims1 = seq(from=1, to=nrow(dataframe)-1, by=1),
+sims2 = seq(from=2, to=nrow(dataframe), by=1)
 )
 
 n <- seq(from=1, to=nrow(sims), by=1)
@@ -30,11 +35,23 @@ lm.sims <- pbapply::pblapply(n, function(x) summary(lm(newY~newX, data=simple.fr
 
 slopes <- unlist(pbapply::pblapply(n, function(x) as.vector(lm.sims[[x]]["coefficients"][[1]][2])))
 
-rsquared <- unlist(pbapply::pblapply(n, function(x) as.vector(lm.sims[[x]]["r.squared"])))
+#rsquared <- unlist(pbapply::pblapply(n, function(x) as.vector(lm.sims[[x]]["r.squared"])))
 
+greater.1 <- which(abs(slopes) > 1)
+
+greater.1[length(greater.1)]
 
 
 }
+
+
+test.frame <- data.frame(
+newy = c(175, 100, 63, 5, 4, 2, 1),
+newx = c(1, 2, 3, 4, 5, 6, 7)
+)
+
+
+screeCrunch(dataframe=test.frame, dependent="newy", independent="newx")
 
 
 
